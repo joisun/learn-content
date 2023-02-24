0135256f4c90b2eef7ff7fa2ff69e418e093834b
jayce <jaycethanks@outlook.com>
2023-02-24 14:19:52 +0800

    chore: learn-react-in-30-days

---
+++ learn-30-Days-Of-React/solutions/src/App.js
@@ -0,0 +1,10 @@
+import HigherOrderComponent from "./page/HigherOrderComponent.page";
+function App() {
+  return (
+    <div className='app'>
+      <HigherOrderComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ learn-30-Days-Of-React/solutions/src/page/HigherOrderComponent.page.jsx
@@ -0,0 +1,8 @@
+function HigherOrderComponent() {
+  return (
+    <div className='wrapper'>
+      <h1>he</h1>
+    </div>
+  );
+}
+export default HigherOrderComponent;
